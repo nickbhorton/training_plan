73b9ddf75993334af45edafe60468ae97f892a74
Nick Horton <nickbhorton40@gmail.com>
2026-02-28 07:53:20 -0700

    fixing hardcoded path error for secret key

--- website/settings.py
+++ website/settings.py
@@ -1,9 +1,10 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-with open("secret_key.txt") as f:
+with open(os.path.join(BASE_DIR, "secret_key.txt")) as f:
     SECRET_KEY = f.read().strip()
 
 DEBUG = True
